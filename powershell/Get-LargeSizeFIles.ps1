6c5573aba093dc54439d34246c2c79a7d0a46910
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-08-14 14:41:03 +0900

    add Get-LargeSizeFiles.ps1

---
+++ powershell/Get-LargeSizeFIles.ps1
@@ -0,0 +1,33 @@
+﻿function Get-LargeSizeFiles {
+    <#
+    .SYNOPSIS
+        サイズが大きいファイルを探し、その一覧を出力する。
+
+    .DESCRIPTION
+        サイズが大きいファイルを探し、その一覧を出力する。
+
+    .EXAMPLE
+        Get-LargeSizeFiles
+
+    .EXAMPLE
+        Get-LargeSizeFiles -Path C:\Users\xxxx\programming -Limit 5
+
+    .PARAMETER Path
+        探索対象のフォルダを指定する。
+        デフォルト値: C:\(Cドライブ直下)
+
+    .PARAMETER Limit
+        サイズが大きいファイルの最大出力数
+        デフォルト値: 10
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string[]]$Path = "C:\",
+        [int]$Limit = 10
+    )
+    PROCESS {
+        Get-ChildItem -Path $Path -Recurse | Select-Object Length,FullName | 
+            Sort-Object -Descending Length | Select-Object -First $Limit
+    }
+}
