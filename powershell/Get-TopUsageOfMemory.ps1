da5d173c8de2f7acc9af8dbb6cba3a3b7c691beb
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-08-16 00:47:51 +0900

    add Get-TopUsageOfMemory.ps1

---
+++ powershell/Get-TopUsageOfMemory.ps1
@@ -0,0 +1,29 @@
+﻿function Get-TopUsageOfMemory {
+    <#
+    .SYNOPSIS
+        メモリ使用率の高いプロセスを出力する。
+
+    .DESCRIPTION
+        メモリ使用率の高いプロセスを出力する。
+
+    .EXAMPLE
+        Get-TopUsageOfMemory
+
+    .EXAMPLE
+        Get-TopUsageOfMemory -Limit 5
+
+    .PARAMETER Limit
+        メモリ使用率の高いプロセスの最大出力数
+        デフォルト値: 10
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [int]$Limit = 10
+    )
+    PROCESS {
+        $result = Get-Process | Select-Object Name, WS | Sort-Object -Descending WS
+        $result | ForEach-Object{$_.WS = $_.WS / 1MB} #メガバイトに変換
+        $result | Select-Object -First $Limit
+    }
+}
