175fe05f5d9b46edb66a1b23bcde6b13edcd7237
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-08-19 23:37:47 +0900

    add Exec-CutLikeUnix.ps1 and Show-InstalledApps.ps1

---
+++ powershell/Exec-CutLikeUnix.ps1
@@ -0,0 +1,41 @@
+﻿function global:Exec-CutLikeUnix {
+<#
+    .SYNOPSIS
+        UnixコマンドライクなCutスクリプト
+    .DESCRIPTION
+        UnixコマンドライクなCutスクリプト
+    .EXAMPLE
+        Exec-CutLikeUnix -Filename C:\Users\test.txt -Field 1
+    .EXAMPLE
+        Exec-CutLikeUnix -Filename C:\Users\test.txt -Delimiter "," -Field 1
+
+    .PARAMETER Filename
+    表示するファイル名(必須)
+
+    .PARAMETER Delimiter
+    区切り文字
+    デフォルト値: " "
+
+    .PARAMETER Field
+    表示する位置番号
+        
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string[]]
+        $Filename,
+
+        [Parameter()]
+        [string[]]
+        $Delimiter = " ",
+
+        [Parameter(Mandatory)]
+        [int]
+        $Field
+    )
+
+    PROCESS {
+        Get-Content -Path $Filename | ForEach-Object {$($_ -split($Delimiter))[$Field-1]}
+    }
+}
---
+++ powershell/Show-InstalledApps.ps1
@@ -0,0 +1,29 @@
+﻿function global:Show-InstalledApps {
+<#
+    .SYNOPSIS
+        インストールされているアプリケーションを表示する。
+    .DESCRIPTION
+        インストールされているアプリケーションを表示する。
+    .EXAMPLE
+        Show-InstalledApps
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [switch]
+        $Descending
+    )
+
+    PROCESS {
+        if ($Descending) { #降順ソート
+            Get-ChildItem -Path('HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall','HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall','HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall') | 
+              ForEach-Object { Get-ItemProperty $_.PsPath | Select-Object DisplayName, DisplayVersion, Publisher } | 
+              Sort-Object DisplayName -Descending
+        } else { #昇順ソート
+            Get-ChildItem -Path('HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall','HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall','HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall') | 
+              ForEach-Object { Get-ItemProperty $_.PsPath | Select-Object DisplayName, DisplayVersion, Publisher } | 
+              Sort-Object DisplayName
+        }
+    }
+}
+
